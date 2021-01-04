c1a4b0c90e23445592e20c192d4844b348c8111f
Mingyu Jin <jmingyu@gmail.com>
2021-01-04 22:53:01 +0900

    Add tests on Square function

--- steps/firstgoal.py
+++ steps/firstgoal.py
@@ -174,8 +174,33 @@ def as_array(x):
 
 """
 # Step 09
-"""
 x = Variable(np.array(0.5))
 y = square(exp(square(x))) # 연속하여 적용
 y.backward()
 print(x.grad)
+"""
+
+import unittest
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+    
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+    
+    def test_gradiant_check(self):
+        x = Variable(np.random.rand(1)) # 무작위 입력값 생성
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+
+unittest.main()
