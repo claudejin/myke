0ad32c46c6ceff25475644b176e59dc9dd439e5a
Mingyu Jin <jmingyu@gmail.com>
2021-01-04 22:33:42 +0900

    Automate back propagation

--- steps/firstgoal.py
+++ steps/firstgoal.py
@@ -2,8 +2,29 @@ import numpy as np
 
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f"The type {type(data)} is not supported.")
+        
         self.data = data
         self.grad = None
+        self.creator = None
+    
+    def set_creator(self, func):
+        self.creator = func
+    
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+        
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop() # 함수를 가져온다
+            x, y = f.input, f.output # 함수의 입력과 출력을 가져온다
+            x.grad = f.backward(y.grad) # backward 메서드를 호출
+            
+            if x.creator is not None:
+                funcs.append(x.creator) # 하나 앞의 함수를 리스트에 추가
 
 """
 # Step 01
@@ -19,8 +40,10 @@ class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x) # 구체적인 계산은 forward 메서드에서 수행
-        output = Variable(y)
+        output = Variable(as_array(y))
+        output.set_creator(self) # 출력 변수에 창조자를 설정
         self.input = input # 입력 변수를 기억(보관)
+        self.output = output # 출력도 저장
         return output
     
     def forward(self, x):
@@ -120,3 +143,39 @@ a.grad = B.backward(b.grad)
 x.grad = A.backward(a.grad)
 print(x.grad)
 """
+
+"""
+# Step 07
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+# 역전파
+y.grad = np.array(1.0)
+y.backward()
+print(x.grad)
+"""
+
+def square(x):
+    return Square()(x) # 한 줄로 작성
+
+def exp(x):
+    return Exp()(x)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+"""
+# Step 09
+"""
+x = Variable(np.array(0.5))
+y = square(exp(square(x))) # 연속하여 적용
+y.backward()
+print(x.grad)
