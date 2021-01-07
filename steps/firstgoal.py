78f58b58b28372620ba4dbb0886e966b2cd19661
Mingyu Jin <jmingyu@gmail.com>
2021-01-07 22:03:09 +0900

    Support multiple inputs (vector)

--- steps/firstgoal.py
+++ steps/firstgoal.py
@@ -13,6 +13,9 @@ class Variable:
     def set_creator(self, func):
         self.creator = func
     
+    def cleargrad(self):
+        self.grad = None
+    
     def backward(self):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
@@ -20,36 +23,39 @@ class Variable:
         funcs = [self.creator]
         while funcs:
             f = funcs.pop() # 함수를 가져온다
-            x, y = f.input, f.output # 함수의 입력과 출력을 가져온다
-            x.grad = f.backward(y.grad) # backward 메서드를 호출
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs, )
             
-            if x.creator is not None:
-                funcs.append(x.creator) # 하나 앞의 함수를 리스트에 추가
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
 
-"""
-# Step 01
-data = np.array(1.0)
-x = Variable(data)
-print(x.data)
-
-x.data = np.array(2.0)
-print(x.data)
-"""
+                if x.creator is not None:
+                    funcs.append(x.creator) # 하나 앞의 함수를 리스트에 추가
 
 class Function:
-    def __call__(self, input):
-        x = input.data
-        y = self.forward(x) # 구체적인 계산은 forward 메서드에서 수행
-        output = Variable(as_array(y))
-        output.set_creator(self) # 출력 변수에 창조자를 설정
-        self.input = input # 입력 변수를 기억(보관)
-        self.output = output # 출력도 저장
-        return output
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs) # 구체적인 계산은 forward 메서드에서 수행
+        if not isinstance(ys, tuple): # 튜플이 아닌 경우 추가 지원
+            ys = (ys, )
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self) # 출력 변수에 창조자를 설정
+        self.inputs = inputs # 입력 변수를 기억(보관)
+        self.outputs = outputs # 출력도 저장
+
+        return outputs if len(outputs) > 1 else outputs[0]
     
-    def forward(self, x):
+    def forward(self, xs):
         raise NotImplementedError()
 
-    def backward(self, gy):
+    def backward(self, gys):
         raise NotImplementedError()
 
 class Square(Function):
@@ -57,44 +63,26 @@ class Square(Function):
         return x ** 2
 
     def backward(self, gy):
-        x = self.input.data
+        x = self.inputs[0].data
         gx = 2 * x * gy
         return gx
-
-"""
-# Step 02
-x = Variable(np.array(10))
-f = Square()
-y = f(x)
-
-print(type(y))
-print(y.data)
-"""
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
     
     def backward(self, gy):
-        x = self.input.data
+        x = self.inputs[0].data
         gx = np.exp(x) * gy
         return gx
 
-"""
-# Step 03
-A = Square()
-B = Exp()
-C = Square()
-
-x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
-print(x.data)
-print(a.data)
-print(b.data)
-print(y.data)
-"""
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+    def backward(self, gy):
+        return gy, gy
 
 def numerical_diff(f, x, eps=1e-4):
     x0 = Variable(x.data - eps)
@@ -103,104 +91,23 @@ def numerical_diff(f, x, eps=1e-4):
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
 
-"""
-# Step 04
-f = Square()
-x = Variable(np.array(2.0))
-dy = numerical_diff(f, x)
-print(dy)
-
-def f(x):
-    A = Square()
-    B = Exp()
-    C = Square()
-    return C(B(A(x)))
-
-x = Variable(np.array(0.5))
-dy = numerical_diff(f, x)
-print(dy)
-"""
-
-"""
-# Step 05
-# There is nothing
-"""
-
-"""
-# Step 06
-A = Square()
-B = Exp()
-C = Square()
-
-x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
-
-y.grad = np.array(1.0)
-b.grad = C.backward(y.grad)
-a.grad = B.backward(b.grad)
-x.grad = A.backward(a.grad)
-print(x.grad)
-"""
-
-"""
-# Step 07
-A = Square()
-B = Exp()
-C = Square()
-
-x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
-
-# 역전파
-y.grad = np.array(1.0)
-y.backward()
-print(x.grad)
-"""
-
 def square(x):
     return Square()(x) # 한 줄로 작성
 
 def exp(x):
     return Exp()(x)
 
+def add(x0, x1):
+    return Add()(x0, x1)
+
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
     return x
 
-"""
-# Step 09
-x = Variable(np.array(0.5))
-y = square(exp(square(x))) # 연속하여 적용
+x = Variable(np.array(3.0))
+y = add(x, x)
+print('y', y.data)
+
 y.backward()
-print(x.grad)
-"""
-
-import unittest
-class SquareTest(unittest.TestCase):
-    def test_forward(self):
-        x = Variable(np.array(2.0))
-        y = square(x)
-        expected = np.array(4.0)
-        self.assertEqual(y.data, expected)
-    
-    def test_backward(self):
-        x = Variable(np.array(3.0))
-        y = square(x)
-        y.backward()
-        expected = np.array(6.0)
-        self.assertEqual(x.grad, expected)
-    
-    def test_gradiant_check(self):
-        x = Variable(np.random.rand(1)) # 무작위 입력값 생성
-        y = square(x)
-        y.backward()
-        num_grad = numerical_diff(square, x)
-        flg = np.allclose(x.grad, num_grad)
-        self.assertTrue(flg)
-
-unittest.main()
+print('x.grad', x.grad)
