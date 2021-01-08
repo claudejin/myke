c09b0e2280a1392ecd4fed797f381ae734f7867b
Mingyu Jin <jmingyu@gmail.com>
2021-01-08 23:46:37 +0900

    Add features by step 22 (with statement, native function supports, properties, memory save, mul, neg, sub, div, pow)

--- steps/firstgoal.py
+++ steps/firstgoal.py
@@ -1,15 +1,58 @@
+import contextlib
+import weakref
 import numpy as np
 
+class Config:
+    enable_backprop = True
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+def no_grad():
+    return using_config('enable_backprop', False)
+
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f"The type {type(data)} is not supported.")
         
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0 # 세대 수를 기록하는 변수
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+    
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
     
     def set_creator(self, func):
         self.creator = func
@@ -18,7 +61,7 @@ class Variable:
     def cleargrad(self):
         self.grad = None
     
-    def backward(self):
+    def backward(self, retain_grad=False):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
         
@@ -35,7 +78,7 @@ class Variable:
 
         while funcs:
             f = funcs.pop() # 함수를 가져온다
-            gys = [output.grad for output in f.outputs]
+            gys = [output().grad for output in f.outputs]
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
                 gxs = (gxs, )
@@ -48,21 +91,28 @@ class Variable:
 
                 if x.creator is not None:
                     add_func(x.creator) # 하나 앞의 함수를 리스트에 추가
+            
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None # y는 약한 참조(weakref)
 
 class Function:
     def __call__(self, *inputs):
+        inputs = [as_variable(x) for x in inputs]
+
         xs = [x.data for x in inputs]
         ys = self.forward(*xs) # 구체적인 계산은 forward 메서드에서 수행
         if not isinstance(ys, tuple): # 튜플이 아닌 경우 추가 지원
             ys = (ys, )
         outputs = [Variable(as_array(y)) for y in ys]
         
-        self.generation = max([x.generation for x in inputs])
-        for output in outputs:
-            output.set_creator(self) # 출력 변수에 창조자를 설정
-        self.inputs = inputs # 입력 변수를 기억(보관)
-        self.outputs = outputs # 출력도 저장
-
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs]) # 세대 설정
+            for output in outputs:
+                output.set_creator(self) # 연결 설정
+            self.inputs = inputs # 입력 변수를 기억(보관)
+            self.outputs = [weakref.ref(output) for output in outputs] # 출력도 저장
+        
         return outputs if len(outputs) > 1 else outputs[0]
     
     def forward(self, xs):
@@ -97,6 +147,55 @@ class Add(Function):
     def backward(self, gy):
         return gy, gy
 
+class Mul(Function):
+    def forward(self, x0, x1):
+        y = x0 * x1
+        return y
+    
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return gy * x1, gy * x0
+
+class Neg(Function):
+    def forward(self, x):
+        return -x
+    
+    def backward(self, gy):
+        return -gy
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+    
+    def backward(self, gy):
+        return gy, -gy
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+    
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+    
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c * x ** (c - 1) * gy
+        return gx
+
 def numerical_diff(f, x, eps=1e-4):
     x0 = Variable(x.data - eps)
     x1 = Variable(x.data + eps)
@@ -113,15 +212,56 @@ def exp(x):
 def add(x0, x1):
     return Add()(x0, x1)
 
+def mul(x0, x1):
+    return Mul()(x0, x1)
+
+def neg(x):
+    return Neg()(x)
+
+def sub(x0, x1):
+    return Sub()(x0, x1)
+
+def rsub(x0, x1):
+    return Sub()(x1, x0) # x0와 x1의 순서를 바꿈
+
+def div(x0, x1):
+    return Div()(x0, x1)
+
+def rdiv(x0, x1):
+    return Div()(x1, x0)
+
+def pow(x, c):
+    return Pow(c)(x)
+
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
     return x
 
-x = Variable(np.array(2.0))
-a = square(x)
-y = add(square(a), square(a))
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(as_array(obj))
+
+Variable.__array_priority__ = 200
+Variable.__add__ = add
+Variable.__radd__ = add
+Variable.__mul__ = mul
+Variable.__rmul__ = mul
+Variable.__neg__ = neg
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__pow__ = pow
+
+x0 = Variable(np.array(3.0))
+x1 = 2.0
+x2 = Variable(np.array(1.0))
+
+y = x0 + x1 + x2
 y.backward()
 
-print(y.data)
-print(x.grad)
+print(y)
+print(y.grad)
+print(x0.grad, x2.grad)
