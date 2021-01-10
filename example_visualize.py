284e63f8c8c050293325121f7c0970e2257a8cff
Mingyu Jin <jmingyu@gmail.com>
2021-01-10 21:35:32 +0900

    Add visualization function and its example

---
+++ example_visualize.py
@@ -0,0 +1,15 @@
+import numpy as np
+from myke import Variable
+from myke.utils import plot_dot_graph
+
+def goldstein(x, y):
+    z = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * \
+        (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
+    return z
+
+x = Variable(np.array(1.0), 'x')
+y = Variable(np.array(1.0), 'y')
+z = goldstein(x, y)
+z.name = 'z'
+
+plot_dot_graph(z, verbose=False)
