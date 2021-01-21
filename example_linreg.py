6a82cabdd5df4464ea92f72cfd169d8d05473758
Mingyu Jin <jmingyu@gmail.com>
2021-01-21 23:30:45 +0900

    Implement linear regression and its example

---
+++ example_linreg.py
@@ -0,0 +1,32 @@
+import numpy as np
+from myke import Variable
+import myke.functions as F
+
+# 토이 데이터셋
+np.random.seed(0) # 시드값 고정
+x = np.random.rand(100, 1)
+y = 5 + 2 * x + np.random.rand(100, 1) # y에 무작위 노이즈 추가
+x, y = Variable(x), Variable(y) # 생략 가능
+
+W = Variable(np.zeros((1, 1)))
+b = Variable(np.zeros(1))
+
+def predict(x):
+    y = F.matmul(x, W) + b
+    return y
+
+lr = 0.1
+iters = 100
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    W.cleargrad()
+    b.cleargrad()
+    loss.backward()
+
+    W.data -= lr * W.grad.data
+    b.data -= lr * b.grad.data
+
+    print(W, b, loss)
