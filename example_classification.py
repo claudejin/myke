2e7fb850136758b4147ddd39a3adc67518cda580
Mingyu Jin <jmingyu@gmail.com>
2021-02-04 22:35:33 +0900

    Introduce Dataset and Transform concepts

--- example_classification.py
+++ example_classification.py
@@ -13,20 +13,20 @@ batch_size = 60
 lr = 0.4
 
 # Data
-x, t = datasets.get_spiral(train=True)
+train_set = datasets.Spiral()
 #plt.scatter(x[:,0], x[:,1], c=t)
 #plt.show()
 model = MLP((10, 10, 3))
 optimizer = optimizers.MomentumSGD(lr, momentum=0.8).setup(model)
 
-data_size = len(x)
+data_size = len(train_set)
 max_iter = math.ceil(data_size / batch_size)
 
 def draw_model():
     # Plot boundary area the model predict
     h = 0.01
-    x_min, x_max = x[:, 0].min() - .1, x[:, 0].max() + .1
-    y_min, y_max = x[:, 1].min() - .1, x[:, 1].max() + .1
+    x_min, x_max = -1, +1
+    y_min, y_max = -1, +1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
     X = np.c_[xx.ravel(), yy.ravel()]
 
@@ -40,9 +40,10 @@ def draw_model():
     N, CLS_NUM = 100, 3
     markers = ['o', 'x', '^']
     colors = ['orange', 'blue', 'green']
-    for i in range(len(x)):
-        c = t[i]
-        plt.scatter(x[i][0], x[i][1], s=40,  marker=markers[c], c=colors[c])
+    for i in range(data_size):
+        x, t = train_set[i]
+        c = t
+        plt.scatter(x[0], x[1], s=40,  marker=markers[c], c=colors[c])
     plt.draw()
     plt.pause(0.01)
     plt.clf()
@@ -54,8 +55,9 @@ for epoch in range(max_epoch):
     for i in range(max_iter):
         # Mini batch
         batch_index = index[i * batch_size:(i + 1) * batch_size]
-        batch_x = x[batch_index]
-        batch_t = t[batch_index]
+        batch = [train_set[i] for i in batch_index]
+        batch_x = np.array([example[0] for example in batch])
+        batch_t = np.array([example[1] for example in batch])
 
         y = model(batch_x)
         loss = F.softmax_cross_entropy_simple(y, batch_t)
